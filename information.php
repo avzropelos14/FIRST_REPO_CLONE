838e1ea26a8ff874fcd2cf5d305502fa7861093f
avzropelos14 <avropelos.student@asiancollege.edu.ph>
2025-04-10 08:35:38 +0800

    Update information.php

--- information.php
+++ information.php
@@ -15,6 +15,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Information</title>
     <style>
+/*changes
+    body{
+            background-color: yellow;
+}
+        */
+
+        
+        
         *{
             font-family: monospace;
         }
